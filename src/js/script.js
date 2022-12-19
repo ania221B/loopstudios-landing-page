26fe4371e0789b86c0e5829007be3b3a47e77423
ania221B <abuwaj@gmail.com>
2022-12-19 21:48:53 +0100

    js tested again

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,8 @@
 const attribution = document.querySelector('.attribution')
 console.log(attribution)
+
+const button = document.querySelector('.navigation-menu-btn')
+
+button.addEventListener('click', e => {
+  console.log('you\'ve clicked the button')
+})
